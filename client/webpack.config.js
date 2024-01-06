0572b8f657b47b13ef52a879a9b16758920450c7
Victoria <sweet.victoria1218@gmail.com>
2024-01-06 12:50:52 -0600

    get manifest file to load

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -16,6 +13,7 @@ module.exports = () => {
     output: {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
+      publicPath: "",
     },
     plugins: [
       new WebpackPwaManifest({
@@ -27,8 +25,7 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
-            size: "500x500",
-            purpose: "maskable",
+            sizes: [96, 144],
           },
         ],
       }),
@@ -40,7 +37,6 @@ module.exports = () => {
         exclude: [/.../, "...", "node_modules"],
         maximumFileSizeToCacheInBytes: 400000,
         swSrc: "./src-sw.js",
-        publicPath: "",
       }),
     ],
 
