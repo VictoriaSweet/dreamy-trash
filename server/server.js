aa045123da916da1045bcbec19793054d216fd59
Victoria <sweet.victoria1218@gmail.com>
2024-01-06 11:49:02 -0600

    added indexeddb

--- server/server.js
+++ server/server.js
@@ -1,12 +1,43 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+const request = window.indexedDB.open("MyTestDatabase", 3);
+request.onerror = (event) => {
+  // Do something with request.errorCode!
+  console.log("meow meow");
+};
+request.onsuccess = (event) => {
+  // Do something with request.result!
+};
+let db;
+const request = indexedDB.open("MyTestDatabase");
+request.onerror = (event) => {
+  console.error("Why didn't you allow my web app to use IndexedDB?!");
+};
+request.onsuccess = (event) => {
+  db = event.target.result;
+};
+db.onerror = (event) => {
+  // Generic error handler for all errors targeted at this database's
+  // requests!
+  console.error(`Database error: ${event.target.errorCode}`);
+};
+
+// This event is only implemented in recent browsers
+request.onupgradeneeded = (event) => {
+  // Save the IDBDatabase interface
+  const db = event.target.result;
+
+  // Create an objectStore for this database
+  const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+};
