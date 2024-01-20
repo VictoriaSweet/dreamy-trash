a9c44c7c33ebf4844840ff94436ef824efdc1455
VictoriaSweet <sweet.victoria1218@gmail.com>
2024-01-20 16:35:48 -0600

    added README.md and cleaned up starter code

--- server/server.js
+++ server/server.js
@@ -10,30 +10,3 @@ app.use(express.json());
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
-
-
-// request.onsuccess = (event) => {
-//   // Do something with request.result!
-// };
-// let db;
-// const request = indexedDB.open("MyTestDatabase");
-// request.onerror = (event) => {
-//   console.error("Why didn't you allow my web app to use IndexedDB?!");
-// };
-// request.onsuccess = (event) => {
-//   db = event.target.result;
-// };
-// db.onerror = (event) => {
-//   // Generic error handler for all errors targeted at this database's
-//   // requests!
-//   console.error(`Database error: ${event.target.errorCode}`);
-// };
-
-// // This event is only implemented in recent browsers
-// request.onupgradeneeded = (event) => {
-//   // Save the IDBDatabase interface
-//   const db = event.target.result;
-
-//   // Create an objectStore for this database
-//   const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
-// };
